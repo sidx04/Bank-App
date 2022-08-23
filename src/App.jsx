d9222bc1124def465d8ba5c70fb805374ebf46e7
Onion-Man <siddpal04@gmail.com>
2022-08-23 22:54:51 +0530

    Initial main-section commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import styles from './styles'
+
+import { Navbar, Billing, Stats, Hero, CardDeal, Testimonials, CTA, Clients, Footer,  Business } from './components'
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-primary w-full overflow-hidden ">
+      <div className={`${styles.padding} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar/>
+        </div>
+      </div>
+
+      <div className={ `bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero></Hero> 
+        </div>
+      </div>
+
+      <div className={ `bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients/>
+          <CTA/>
+          <Footer/> 
+        </div>
+      </div>
+
+    </div>
   )
 }
 
