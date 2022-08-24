8070fd58c5898bc2673626d37bec42fe61698ff4
Onion-Man <siddpal04@gmail.com>
2022-08-25 00:51:21 +0530

    Finished Billing Component

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -9,7 +9,23 @@ const Billing = () => {
         <div className="absolute z-[3] -left-1/2 top-0 w-[60%] h-[50%] rounded-full white__gradient"/>
         <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full blue__gradient"/>
       </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className="sm:block hidden"/>
+          billing and invoicing.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] font-signika`}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore voluptates recusandae dolorum, aliquam officiis, sequi obcaecati eos saepe ex, adipisci cumque molestias iste suscipit explicabo? Tempora voluptatibus neque nobis commodi.
+        </p>
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img src={apple} alt="appstore" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"></img>
+          <img src={google} alt="playstore" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"></img>
+        </div>
+      </div>
     </section>
+
+    
   )
 }
 
